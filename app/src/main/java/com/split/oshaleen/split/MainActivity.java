bf093bd39615d1179fc9c4b2bd18e89773778694
osguus <ovg@andrew.cmu.edu>
2018-02-10 07:40:36 -0500

    Initial commit

---
+++ app/src/main/java/com/split/oshaleen/split/MainActivity.java
@@ -0,0 +1,65 @@
+package com.split.oshaleen.split;
+
+//import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment frag = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    frag = ListFragment.newInstance();
+                    break;
+                case R.id.navigation_dashboard:
+                    frag = BuyFragment.newInstance();
+                    break;
+                case R.id.navigation_notifications:
+                    frag = SplitFragment.newInstance();
+                    break;
+            }
+
+            if(frag != null){
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.container, frag);
+                ft.addToBackStack(null);
+                ft.commit();
+                return true;
+            }
+
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set home fragment as initial fragment
+        Fragment frag = HomeFragment.newInstance();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, frag, frag.getTag());
+        ft.commit();
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
